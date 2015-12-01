a28d39d1e8edafe2842dad870ddd5ca84884e21f
Chris Kelner <Chris.Kelner@gmail.com>
2015-12-01 20:38:59 +0000

    Add tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,9 +1,22 @@
 require 'spec_helper'
 
 describe 'apache::default' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
+
+  describe port(80) do
+    it { should be_listening }
   end
+
+  describe command('curl http://localhost') do
+    its(:stdout) { should match /Hello world!/ }
+  end
+
+  describe package('httpd') do
+    it { should be_installed }
+  end
+
+  describe service('httpd') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
 end
